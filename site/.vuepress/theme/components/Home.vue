3e4402f61cfe78e66e540161f64ea86b5fbb6b36
monikappv <monika.popova@datopian.com>
2020-04-29 01:51:53 +0300

    [refactor][xl]: Refactoring to vuepress.

---
+++ site/.vuepress/theme/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <h1 class=""> {{ $page.frontmatter.title }} </h1>
+    <h2 class=""> {{ $page.frontmatter.subtitle }} </h2>
+    <Content />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default {
+  computed: {
+    data() {
+      return this.$page.frontmatter;
+    },
+  }
+};
+</script>
---
+++ site/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-full">
+    <div class="inner-container pt-24 mx-auto">
+      <Home v-if="$page.frontmatter.layout === 'home'" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Home from "../components/Home.vue";
+
+export default {
+  components: {
+    Home
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+.inner-container {
+  max-width: 56rem;
+}
+
+</style>
