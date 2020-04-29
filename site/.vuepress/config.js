ddfc1758f74cb10e10d6fc82e36fceb9afd6f1a8
Rufus Pollock <rufus.pollock@datopian.com>
2020-04-29 22:13:49 +0200

    [theme][xs]: one more go at fixing css build.

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,6 +1,6 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ["./.vuepress/theme/**/*.*", "./!(node_modules)/**/*.md", "./*.md"],
+  content: ["./site/.vuepress/theme/**/*.*", "./!(node_modules)/**/*.md", "./*.md"],
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
