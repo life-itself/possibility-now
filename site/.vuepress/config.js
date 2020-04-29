fb272ea95ffe726fab48ba57dda06b9e86e8a01d
Rufus Pollock <rufus.pollock@datopian.com>
2020-04-29 21:24:25 +0200

    [theme][s]: move postcss config including cssnano into vuepress config.

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,3 +1,10 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./.vuepress/theme/**/*.*", "./!(node_modules)/**/*.md", "./*.md"],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
 module.exports = {
   title: "Art / Earth / Tech",
   description: "",
@@ -5,7 +12,10 @@ module.exports = {
   postcss: {
     plugins: [
       require("tailwindcss")("./tailwind.config.js"),
-      require("autoprefixer")
+      require('autoprefixer'),
+      ...process.env.NODE_ENV === 'production'
+        ? [purgecss, require('cssnano')]
+        : []
     ]
   },
   themeConfig: {
