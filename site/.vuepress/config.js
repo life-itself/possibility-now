2febb4cbcccafa457cdba5350c0c9b517294cf9d
monikappv <monika.popova@datopian.com>
2020-04-30 16:18:14 +0300

    [SEO][s]: Install SEO plugin; reinstate meta tags incl. image.

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -10,8 +10,7 @@ module.exports = {
   description: "In this moment of crisis, a space of possibility is opening up. Let's sustain and grow it!",
   head: [
     ['link', { rel: "icon", href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌻</text></svg>"}],
-    ['link', { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,400i,500,700&display=swap" }],
-    ['meta', {property: "og:image", content: "https://possibilitynow.org/img/possibilitynow.png" }]
+    ['link', { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,400i,500,700&display=swap" }]
   ],
   dest: "public",
   postcss: {
@@ -34,6 +33,15 @@ module.exports = {
       '@vuepress/google-analytics',
       {
         'ga': 'UA-102022428-4'
+      }
+    ],
+    [
+      '@limdongjin/vuepress-plugin-simple-seo', 
+      {
+      default_image: 'https://possibilitynow.org/img/possibilitynow.png',
+      root_url: 'https://possibilitynow.org/',
+      default_site_name: 'Possibility Now! A different future is possible.',
+      description: "In this moment of crisis, a space of possibility is opening up. Let's sustain and grow it!"
       }
     ]
   ]
