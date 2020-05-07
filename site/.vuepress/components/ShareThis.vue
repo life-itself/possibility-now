11f61b0f7cf8ebcb15ee624032b7c256068e9b86
monikappv <monika.popova@datopian.com>
2020-05-07 15:48:22 +0300

    [css][s]: Replace normal css with tailwind for share links and replace section with div tag for the share section.

--- site/.vuepress/components/ShareThis.vue
+++ site/.vuepress/components/ShareThis.vue
@@ -1,15 +1,15 @@
 <template>
-  <section id="share-section" class="text-center pt-20">
+  <div id="share-section" class="text-center">
     <img class="share-icon mx-auto pb-10" src="/img/share.svg"/>
     <h3>Share Possibility Now!</h3>
       <div class="container text-2xl py-8">
-        <a class="px-6 share-link" href="http://twitter.com/intent/tweet?text=A+different+future+is+possible%21;url=https%3A%2F%2Fwww.possibilitynow.org" target="_blank" rel="nofollow noopener">Twitter</a>
-        <a href="#share-section" class="share-link" v-clipboard:copy="thingToCopy" v-clipboard:success="onCopy" v-clipboard:error="onError">
+        <a class="px-6 hover:underline" href="http://twitter.com/intent/tweet?text=A+different+future+is+possible%21;url=https%3A%2F%2Fwww.possibilitynow.org" target="_blank" rel="nofollow noopener">Twitter</a>
+        <a href="#share-section" class="hover:underline" v-clipboard:copy="thingToCopy" v-clipboard:success="onCopy" v-clipboard:error="onError">
           <span id="toBeCopied"> Copy to Clipboard </span>
         </a>
-        <a class="px-6 share-link" target="_blank" rel="nofollow noopener" href="mailto:?body=https%3A%2F%2Fwww.possibilitynow.org">Email</a>
+        <a class="px-6 hover:underline" target="_blank" rel="nofollow noopener" href="mailto:?body=https%3A%2F%2Fwww.possibilitynow.org">Email</a>
       </div>
-  </section>
+  </div>
 </template>
 
 <script>
