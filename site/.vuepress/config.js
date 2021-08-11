c37cbb680059fe747766c01296c5910147db0823
Rufus Pollock <rufus.pollock@datopian.com>
2021-08-11 14:35:20 +0200

    [theme][xs]: switch back to original sunflower logo.

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -24,7 +24,7 @@ module.exports = {
     ]
   },
   themeConfig: {
-    logo: "/img/possibility-now-new-logo.svg",
+    logo: "/img/possibility-now-logo.svg",
     docsDir: "site",
     editLinks: true,
     smoothScroll: true,
